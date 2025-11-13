dd62bfd8c6aeed53818ffa52cef5c1716ac74485
justifyin <96999619+justifyin@users.noreply.github.com>
2025-11-13 23:11:26 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ SELECT
     e.first_name || ' ' || e.last_name AS seller,
     COUNT(*) AS operations,
     FLOOR(SUM(p.price * s.quantity)) AS income  -- отбрасываем дробную часть
-FROM sales AS s
+FROM salesdb.sales AS s
 JOIN salesdb.products AS p
     ON s.product_id = p.product_id
 JOIN employees AS e
