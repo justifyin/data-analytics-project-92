aad3a75329758c96151993e67ed0b70e07d17220
justifyin <96999619+justifyin@users.noreply.github.com>
2025-11-13 22:56:28 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
 /* Считаем общее количество покупателей из таблицы customers */
-SELECT
-    COUNT(*) AS customers_count
+SELECT COUNT(*) AS customers_count
 FROM customers;
 
 /* Считаем 10 лучших продавцов по суммарной выручке */
